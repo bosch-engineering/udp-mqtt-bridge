6ff8304281eeab36dca4310aaabc2972fb4bb07f
Bernhard Rode <mail@bernhardrode.de>
2024-09-05 07:42:17 +0000

    feat: relay raw messages over mqtt (#23)

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"runtime"
-	"time"
 	"udp_mqtt_bridge/utils"
 
 	"github.com/eiannone/keyboard"
@@ -21,7 +20,7 @@ func main() {
 	slog.SetLogLevel(logLevel)
 
 	// Map to store timestamps for CloudEvent IDs
-	eventTimestamps := make(map[string]time.Time)
+	// eventTimestamps := make(map[string]time.Time)
 
 	// Initialize UDP and MQTT
 	udpConn, err := utils.NewConnection(config.UDP.IpIn, config.UDP.PortIn)
@@ -50,37 +49,11 @@ func main() {
 		for {
 			select {
 			case udpPacket := <-udpConn.Receive():
-				// Unmarshal the UDP packet into a CloudEvent
-				ce, err := utils.UnmarshalCloudEvent(udpPacket)
-				if err != nil {
-					slog.Debugf("Error unmarshalling CloudEvent via UDP: %v", err)
-					continue
-				}
-				slog.Debugf("Forwarding CloudEvent from UDP to MQTT: %s %s", ce.ID(), ce.Type())
-
-				mqttClient.Send(config.MQTT.TopicOut, ce)
+				// udpConn.SendRaw(config.UDP.IpOut, config.UDP.PortOut, udpPacket)
+				mqttClient.SendRaw(config.MQTT.TopicOut, udpPacket)
 
 			case mqttMsg := <-mqttClient.Receive():
-				slog.Debugf("Received MQTT message: %s", string(mqttMsg))
-				// Unmarshal the UDP packet into a CloudEvent
-				ce, err := utils.UnmarshalCloudEvent(mqttMsg)
-				if err != nil {
-					slog.Debugf("Error unmarshalling CloudEvent via MQTT: %v", err)
-					continue
-				}
-				slog.Debugf("Received CloudEvent via MQTT: %s %s", ce.ID(), ce.Type())
-
-				// Calculate and log the duration
-				if startTime, ok := eventTimestamps[ce.ID()]; ok {
-					duration := time.Since(startTime)
-					slog.Debugf("Duration for CloudEvent ID: %s %s - %v", ce.ID(), ce.Type(), duration)
-					// Optionally, remove the entry from the map if no longer needed
-					delete(eventTimestamps, ce.ID())
-				}
-
-				slog.Debugf("Forwarding CloudEvent from MQTT to UDP: %s %s", ce.ID(), ce.Type())
-				// Process the MQTT message and possibly send it to UDP
-				udpConn.Send(config.UDP.IpOut, config.UDP.PortOut, ce)
+				udpConn.SendRaw(config.UDP.IpOut, config.UDP.PortOut, mqttMsg)
 			}
 		}
 	}()
@@ -88,18 +61,7 @@ func main() {
 	// Capture keyboard input and send UDP ping message on space bar press
 	for {
 		char, key, _ := keyboard.GetKey()
-		if key == keyboard.KeySpace {
-			ce, err := utils.CreateCloudEvent("com.bosch-engineering.ping", "https://bosch-engineering.com", "ping")
-			if err != nil {
-				return
-			}
-			slog.Println("Space bar pressed, sending ping package via UDP...")
-
-			udpConn.Send(config.UDP.IpOut, config.UDP.PortOut, ce)
-			eventTimestamps[ce.ID()] = time.Now()
-		}
 		if char == 'q' || key == keyboard.KeyEsc || (key == keyboard.KeyCtrlC && runtime.GOOS != "windows") {
-			slog.Println("Exiting...")
 			break
 		}
 	}
